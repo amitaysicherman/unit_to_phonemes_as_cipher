06826f3be37201b391798e030a23605fd9c0e67c
amitaysicherman <95076948+amitaysicherman@users.noreply.github.com>
2023-05-09 10:33:00 +0000

    add scripy to eval key

---
+++ eval_key.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import pandas as pd
+
+
+def get_uncode_values(name):
+    """get_uncode_values [summary]"""
+    with open(f"texts/{name}_sorted.txt", "r") as f:
+        mapping = f.read().splitlines()
+    mapping = {mapping[i]: i for i in range(len(mapping))}
+
+    with open(f'texts/{name}.txt', "r") as f:
+        symbols = f.read().splitlines()
+    symbols = [mapping[p] for p in " ## ".join(symbols).split()]
+    return np.array(symbols)
+
+
+phonemes = get_uncode_values("phonemes")
+code = get_uncode_values("code")
+
+base_scores="scores"
+for file_name in os.listdir(base_scores):
+    if  not file_name.startswith("best_key"):
+        continue
+    with open(f'{base_scores}/{file_name}') as f:
+        mapping=f.read().strip().split(",")
+    correct=0
+    for p,c in zip(phonemes,code):
+        if int(mapping[c])==p:
+            correct+=1
+    print(file_name,' Phonemes Error:',1-correct/len(phonemes))
--- get_superv_count.py
+++ get_superv_count.py
@@ -19,17 +19,20 @@ phonemes_uniqe=np.unique(phonemes)
 
 code = get_uncode_values("code")
 code_uniqe=np.unique(code)
-
+print(code_uniqe)
+print(len(code_uniqe))
 superv_map=[""]*len(code_uniqe)
 superv_count=[0]*len(phonemes_uniqe)
-
+print(len(superv_map))
 for c in code_uniqe:
     phonemes_in_code=phonemes[code==c]
     max_phonemes=np.unique(phonemes_in_code,return_counts=True)[0][np.argmax(np.unique(phonemes_in_code,return_counts=True)[1])]
     superv_map[c]=str(max_phonemes)
     superv_count[max_phonemes]+=1
+print(",".join(superv_map))
+print(len(",".join(superv_map).split(",")))
 with open("texts/superv_map.txt","w") as f:
-    f.write("\n".join(superv_map))
+    f.write(",".join(superv_map))
 
 with open("texts/super_count.txt","w") as f:
     f.write(",".join([str(x) for x in superv_count]))
